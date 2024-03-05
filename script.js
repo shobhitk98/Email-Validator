fc271bebcdb88b153f7342d31b0ab67e5a5d88b7
shobhitk98 <159898483+shobhitk98@users.noreply.github.com>
2024-03-05 22:01:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log("this is email validator");
+let result = {
+    "tag": "",
+    "free": false,
+    "role": false,
+    "user": "akshaykumar",
+    "email": "akshaykumar@codewithharry.com",
+    "score": 0.64,
+    "state": "undeliverable",
+    "domain": "codewithharry.com",
+    "reason": "invalid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": false,
+    "did_you_mean": "",
+    "format_valid": true
+}
+submitBtn.addEventListener ("click", async(e)=>{
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML=` <img width="93" src="img/loading.svg" alt="">`
+     let key = "ema_live_qOfpDfyIywuGUm8pKX9aTw8qFREl3OoO4BniGiVT"
+    let email= document.getElementById("username").value
+let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+let res = await fetch(url)
+    let result = await res.json()
+   
+let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
+
+
+
+// submitBtn.addEventListener("click", async (e) => {
+//     e.preventDefault()
+//     console.log("Clicked!")
+//     resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+//     let key = "ema_live_ITkV5Pqq40WNdMLdjqMZlNcb4AEBrNvWiSFd1MYE"
+//     let email = document.getElementById("username").value
+//     let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+//     let res = await fetch(url)
+//     let result = await res.json()
+   
